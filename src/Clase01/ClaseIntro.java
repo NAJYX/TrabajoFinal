aa17d52e3ed8da70c393ad1d646c4b6e5086a23f
NAJYX <166097506+NAJYX@users.noreply.github.com>
2024-04-13 23:12:21 -0500

    Automático

--- src/Clase01/ClaseIntro.java
+++ src/Clase01/ClaseIntro.java
@@ -7,7 +7,7 @@ class Intro {
 
         System.out.println("Hola Dogorcito, no me cagues");
 
-        String nombre = "tercer canbio";
+        String nombre = "4to canbio";
 
         int edad = 26;
         double talla = 1.72;
